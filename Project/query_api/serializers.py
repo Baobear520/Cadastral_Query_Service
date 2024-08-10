9a7d14ba35cef8220e5306851d00861c1db0bceb
Dima <aldmikon@gmail.com>
2024-08-10 12:44:21 +0800

    6th commit Adding rest-framework to INSTALLED_APS Creating serializers

---
+++ Project/query_api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer
+
+from query_api.models import Query,Results
+
+
+class QuerySerializer(ModelSerializer):
+    """A serializer class for the Query model"""
+
+    class Meta:
+        model = Query
+        fields = ['id','cadastral_number','lattitude','longtitude','created_at','updated_at']
+        
+
+class ResultSerializer(ModelSerializer):
+    """A serializer class for the Results model"""
+
+    class Meta:
+        model = Results
+        fields = ['id','query','result','created_at','updated_at']
+

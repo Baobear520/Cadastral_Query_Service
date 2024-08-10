f1b4ce378b219f2ae5fdf06570ef7a2801cfe706
Dima <aldmikon@gmail.com>
2024-08-10 11:27:13 +0800

    5th commit Creating query_api.urls Setting up urlconf

---
+++ Project/query_api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from query_api import views
+
+
+urlpatterns = [
+    #path('query/', views),
+    #path('result/', views),
+    #path('ping/', views),
+    #path('history/', views),
+
+]

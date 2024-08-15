19140fe0bb7ed17b612a7aeec49f8d914effa77c
Dima <aldmikon@gmail.com>
2024-08-15 13:43:14 +0800

    17th commit Refactoring serialization (adding HistorySerializer)

--- Project/query_api/serializers.py
+++ Project/query_api/serializers.py
@@ -2,9 +2,15 @@ from rest_framework.serializers import ModelSerializer
 
 from query_api.models import Query
 
-
 class QuerySerializer(ModelSerializer):
     """A serializer class for the Query model"""
+
+    class Meta:
+        model = Query
+        fields = ['id','cadastral_number','latitude','longtitude']
+
+class HistorySerializer(ModelSerializer):
+    """A serializer class for representing Query model at /history endpoint """
 
     class Meta:
         model = Query

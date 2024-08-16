dda890cb12e3c1eabd09111ffcda54a1169d0f36
Dima <aldmikon@gmail.com>
2024-08-16 09:38:56 +0800

    23rd commit Refactoring the get method of ResultView

--- Third_party_server/property_api/views.py
+++ Third_party_server/property_api/views.py
@@ -1,5 +1,4 @@
-import random, time
-from django.utils.timezone import now
+import random
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,8 +6,8 @@ from rest_framework.views import APIView
 class PropertyView(APIView):
 
     def get(self, request):
-        time.sleep(random.randint(1,5))
-
+        cadastral_number = request.query_params.get('cadastral_number')
+        print(f"Performing a lookup for cadastral_number {cadastral_number}")
         result = random.choice([True,False])
 
         return Response({"result":result},status=status.HTTP_200_OK)
